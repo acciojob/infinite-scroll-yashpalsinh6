a2113aee8e844d539e64b09b9e44c233ad3aa2dc
yashpalsinh6 <75825134+yashpalsinh6@users.noreply.github.com>
2024-05-01 14:58:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,21 @@
-//your code here!
+let list = document.querySelector("#infi-list");
+// console.log(list);
 
+for(let i=1; i<=10; i++)
+{
+  list.innerHTML += `<li>Item ${i}</li>`
+}
+
+let n = 10;
+
+list.addEventListener("scroll", ()=>{
+ 
+  if(list.scrollHeight - list.scrollTop - list.clientHeight < 1){
+ 
+    list.innerHTML += `
+    <li>Item ${n+1}</li>
+    <li>Item ${n+2}</li>
+    `
+    n=n+2;
+  }
+})
